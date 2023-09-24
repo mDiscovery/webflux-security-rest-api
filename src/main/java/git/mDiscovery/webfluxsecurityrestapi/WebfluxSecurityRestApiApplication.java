d2258226bf36117704792db648ba3eeb591f990d
MI <80389747+mi1703@users.noreply.github.com>
2023-09-24 19:47:40 +0100

    Initial commit of Spring Initializr

---
+++ src/main/java/git/mDiscovery/webfluxsecurityrestapi/WebfluxSecurityRestApiApplication.java
@@ -0,0 +1,13 @@
+package git.mDiscovery.webfluxsecurityrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxSecurityRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxSecurityRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/git/mDiscovery/webfluxsecurityrestapi/WebfluxSecurityRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package git.mDiscovery.webfluxsecurityrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxSecurityRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
