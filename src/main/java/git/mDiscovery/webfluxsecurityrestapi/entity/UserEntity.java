f34bdc76cad5028421fc238b7ebfb6b43b2a7681
MI <maksym1703@gmail.com>
2023-10-01 22:26:11 +0100

    feat: annotate UserEntity id field as @Id

--- src/main/java/git/mDiscovery/webfluxsecurityrestapi/entity/UserEntity.java
+++ src/main/java/git/mDiscovery/webfluxsecurityrestapi/entity/UserEntity.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Table("users")
 public class UserEntity {
+    @Id
     private Long id;
     private String username;
     private String password;
