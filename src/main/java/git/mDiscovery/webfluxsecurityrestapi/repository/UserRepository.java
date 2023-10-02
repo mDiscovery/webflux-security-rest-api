874eb24898f31cc2cc47418b6d94f6dbe8c3062d
MI <maksym1703@gmail.com>
2023-10-02 11:22:03 +0100

    feat: specify UserRepository to extend R2dbcRepository

--- src/main/java/git/mDiscovery/webfluxsecurityrestapi/repository/UserRepository.java
+++ src/main/java/git/mDiscovery/webfluxsecurityrestapi/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package git.mDiscovery.webfluxsecurityrestapi.repository;
 
-public interface UserRepository {
+import git.mDiscovery.webfluxsecurityrestapi.entity.UserEntity;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+public interface UserRepository extends R2dbcRepository<UserEntity, Long> {
 }
