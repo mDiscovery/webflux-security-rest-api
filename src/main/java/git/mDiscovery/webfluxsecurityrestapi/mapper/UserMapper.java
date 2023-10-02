b909f928244b541d6fca3e674e64b038fe3554ab
MI <maksym1703@gmail.com>
2023-10-02 11:08:32 +0100

    feat: add UserEntity/UserDto map() to UserMapper

--- src/main/java/git/mDiscovery/webfluxsecurityrestapi/mapper/UserMapper.java
+++ src/main/java/git/mDiscovery/webfluxsecurityrestapi/mapper/UserMapper.java
@@ -1,4 +1,9 @@
 package git.mDiscovery.webfluxsecurityrestapi.mapper;
 
+import git.mDiscovery.webfluxsecurityrestapi.dto.UserDto;
+import git.mDiscovery.webfluxsecurityrestapi.entity.UserEntity;
+
 public interface UserMapper {
+    UserDto map(UserEntity userEntity);
+    UserEntity map(UserDto userDto);
 }
