3cbc2810dfdc78984fc4851973f19c7667bc919a
MI <maksym1703@gmail.com>
2023-10-02 11:13:37 +0100

    feat: annotate UserEntity return map() with @InheritInverseConfiguration

--- src/main/java/git/mDiscovery/webfluxsecurityrestapi/mapper/UserMapper.java
+++ src/main/java/git/mDiscovery/webfluxsecurityrestapi/mapper/UserMapper.java
@@ -2,10 +2,12 @@ package git.mDiscovery.webfluxsecurityrestapi.mapper;
 
 import git.mDiscovery.webfluxsecurityrestapi.dto.UserDto;
 import git.mDiscovery.webfluxsecurityrestapi.entity.UserEntity;
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     UserDto map(UserEntity userEntity);
+    @InheritInverseConfiguration
     UserEntity map(UserDto userDto);
 }
