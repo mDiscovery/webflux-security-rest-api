0ff1578f0ea16da9a9583cfd7c654418a4b8f75b
MI <maksym1703@gmail.com>
2023-10-02 11:11:20 +0100

    feat: annotate UserMapper with @Mapper

--- src/main/java/git/mDiscovery/webfluxsecurityrestapi/mapper/UserMapper.java
+++ src/main/java/git/mDiscovery/webfluxsecurityrestapi/mapper/UserMapper.java
@@ -2,7 +2,9 @@ package git.mDiscovery.webfluxsecurityrestapi.mapper;
 
 import git.mDiscovery.webfluxsecurityrestapi.dto.UserDto;
 import git.mDiscovery.webfluxsecurityrestapi.entity.UserEntity;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface UserMapper {
     UserDto map(UserEntity userEntity);
     UserEntity map(UserDto userDto);
