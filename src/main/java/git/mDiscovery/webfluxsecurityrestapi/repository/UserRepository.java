9c7f22dafba0dc1eff53ecf9d266f25147c29403
MI <maksym1703@gmail.com>
2023-10-02 11:25:44 +0100

    feat: add findByUsername() to UserRepository

--- src/main/java/git/mDiscovery/webfluxsecurityrestapi/repository/UserRepository.java
+++ src/main/java/git/mDiscovery/webfluxsecurityrestapi/repository/UserRepository.java
@@ -2,6 +2,8 @@ package git.mDiscovery.webfluxsecurityrestapi.repository;
 
 import git.mDiscovery.webfluxsecurityrestapi.entity.UserEntity;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Mono;
 
 public interface UserRepository extends R2dbcRepository<UserEntity, Long> {
+    Mono<UserEntity> findByUsername(String username);
 }
