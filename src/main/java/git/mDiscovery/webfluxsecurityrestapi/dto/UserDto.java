7fe5a3843dac14e9309ae3450079abdb140fb557
MI <maksym1703@gmail.com>
2023-10-02 10:18:17 +0100

    feat: annotate UserDto password with @JsonProperty access WRITE_ONLY

--- src/main/java/git/mDiscovery/webfluxsecurityrestapi/dto/UserDto.java
+++ src/main/java/git/mDiscovery/webfluxsecurityrestapi/dto/UserDto.java
@@ -1,5 +1,6 @@
 package git.mDiscovery.webfluxsecurityrestapi.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import git.mDiscovery.webfluxsecurityrestapi.entity.UserRole;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 public class UserDto {
     private Long id;
     private String username;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private UserRole role;
     private String firstName;
