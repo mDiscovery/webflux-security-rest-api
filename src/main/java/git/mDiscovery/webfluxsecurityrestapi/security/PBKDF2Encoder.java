9d6ec04beaaf2d2a2a5202295a627dddc99d4232
MI <maksym1703@gmail.com>
2023-10-03 12:04:04 +0100

    feat(JWT): specify PBKDF2Encoder to implement PasswordEncoder

--- src/main/java/git/mDiscovery/webfluxsecurityrestapi/security/PBKDF2Encoder.java
+++ src/main/java/git/mDiscovery/webfluxsecurityrestapi/security/PBKDF2Encoder.java
@@ -1,4 +1,15 @@
 package git.mDiscovery.webfluxsecurityrestapi.security;
 
-public class PBKDF2Encoder {
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class PBKDF2Encoder implements PasswordEncoder {
+    @Override
+    public String encode(CharSequence rawPassword) {
+        return "";
+    }
+
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return false;
+    }
 }
