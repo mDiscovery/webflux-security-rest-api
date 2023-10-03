cb87dc831fc118693075600494d2d9a0b671744d
MI <maksym1703@gmail.com>
2023-10-03 12:18:17 +0100

    feat(JWT): add SECRET_KEY_INSTANCE filed to PBKDF2Encoder Note: entity/approach to generate token

--- src/main/java/git/mDiscovery/webfluxsecurityrestapi/security/PBKDF2Encoder.java
+++ src/main/java/git/mDiscovery/webfluxsecurityrestapi/security/PBKDF2Encoder.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class PBKDF2Encoder implements PasswordEncoder {
+    private static final String SECRET_KEY_INSTANCE = "PBKDF2WithHmacSHA512";
     @Override
     public String encode(CharSequence rawPassword) {
         return "";
