f543b39eab4c9e8c6e2e69309207006661802237
MI <maksym1703@gmail.com>
2023-10-05 15:40:17 +0100

    feat(JWT): inject password encoder configs to PBKDF2Encoder

--- src/main/java/git/mDiscovery/webfluxsecurityrestapi/security/PBKDF2Encoder.java
+++ src/main/java/git/mDiscovery/webfluxsecurityrestapi/security/PBKDF2Encoder.java
@@ -1,11 +1,19 @@
 package git.mDiscovery.webfluxsecurityrestapi.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PBKDF2Encoder implements PasswordEncoder {
     private static final String SECRET_KEY_INSTANCE = "PBKDF2WithHmacSHA512";
+
+    @Value("${jwt.password.encoder.secret}")
+    private String secret;
+    @Value("${jwt.password.encoder.iterations}")
+    private Integer iterations;
+    @Value("${jwt.password.encoder.keylength}")
+    private Integer keyLength;
     @Override
     public String encode(CharSequence rawPassword) {
         return "";
